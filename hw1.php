487e9de3cf4c6f179d7821ad3a12800d2f7c9aef
Sadat <laptopparadise@Sadat-2.local>
2025-07-23 13:35:52 +0600

    Initial update for Bank Management

---
+++ hw1.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AIUB</title>
+</head>
+<body>
+    <center> <h1 style= "color: #E0FFFF;"> Bank Management System </h1>
+<h2 style ="color: #E0FFFF;">Your trusted Financial Partner</h2>
+
+</center>
+
+    <table>
+       <tr><center><h4>Customer Registration Form</h4></center></tr>
+            
+        <tr>
+            <td>Full Name: <br>
+                <input type="text">
+            </td>
+            
+        </tr>
+        <br>
+        <tr>
+            <td>Date of Birth: <br>
+                <br>
+                <input type="date">
+            </td>
+        </tr>
+
+<br>
+        <tr>
+            <td>
+                Gender: <br>
+                <input type="radio" name=gender value=male> Male
+                <input type="radio" name=gender value=female>Female
+                <input type="radio" name=gender value=other>Other
+            </td>
+        </tr>
+<br>
+        <tr>
+            <td>
+                 Marital status: <br>
+                    <select name ="marital status">
+                        <option value="single">Single</option>
+                        <option value="Married">Married</option>
+                        <option value="Devorced">Devorced</option>
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                 Account type: <br>
+                    <select name ="Savings">
+                        <option >Current</option>
+                        <option >Deposite</option>
+                       
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                Initial Deposit Amount: <br>
+                <input type="text">
+            </td>
+        </tr>
+
+         <tr>
+            <td>
+                Enter Mobile Number: <br>
+                <input type="text">
+            </td>
+        </tr>
+
+         <tr>
+            <td>
+                Email Adress: <br>
+                <input type="text">
+            </td>
+        </tr>
+
+         <tr>
+            <td>
+                Adress: <br>
+                <textarea rows="4" cols="50" placeholder="Enter your address here..."></textarea>
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                Occupation: <br>
+                <input type="text">
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                National ID (NID): <br>
+                <input type="text">
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                Set Password: <br>
+                <input type="text">
+            </td>
+        </tr>
+
+        <tr>
+            <td>Upload ID Proof: <br>
+                <input type="file">
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <input type="checkbox">I agree to the terms and conditions
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <button type="Submit">Submit</button>
+                <button type="Reset">Reset</button>
+            </td>
+        </tr>
+
+    </table>
+       
+   
+
+
+   
+    <style>
+
+        body{background-color: #008B8B;}
+
+        h1,h2, {
+            color: white;
+            text-align:center;
+        }
+        table
+        {
+            background-color: #E0FFFF;
+            border: 2px solid #000;
+            width: 50%;
+            margin: auto;
+            padding: 20px;
+
+        }
+
+        input[type=submit],inpur[type=reset]
+        {
+            padding:5px;
+        }
+
+
+    </sBank>
+
+  
+
+
+</body>
+</html>
